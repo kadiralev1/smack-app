7f70b4ec2b9fc7f5f8beba790f08e07434918645
kadiralev1 <38442381+kadiralev1@users.noreply.github.com>
2018-07-31 00:27:13 +0300

    added login process

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -18,7 +18,7 @@ typealias CompletionHandler = (_ Success:Bool) -> ()
 
 let BASE_URL = "https://chatapppp.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
-
+let URL_LOGIN = "\(BASE_URL)account/login"
 
 // SEGUES
 
@@ -32,3 +32,9 @@ let UNWIND = "unwindToChannel"
 let TOKEN_KEY = "token"
 let LOGGED_IN_KEY = "loggedIn"
 let USER_EMAIL = "userEmail"
+
+
+// Headers
+let HEADER = [
+    "Content-Type" : "application/json; charset=utf-8"
+]
